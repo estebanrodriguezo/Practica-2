5da23804d225b8817628b345c8597fdc3266548f
estebanrodriguezo <esteban.rodriguezo@udea.edu.co>
2018-03-15 13:25:35 -0500

    practica #2

---
+++ Problema-5/main.cpp
@@ -0,0 +1,77 @@
+/*Problema 5. Haga una función que reciba un numero entero (int) y lo convierta a cadena de caracteres. Use
+parámetros por referencia para retornar la cadena. Escriba un programa de prueba.
+Ejemplo: si recibe un int con valor 123, la cadena que se retorne debe ser “123”.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+static int printdig(int numero,int nDigitos);
+static int potencia(int base, int exp);
+int main()
+{
+
+    int x,nDigitos=1,z;
+    cout << "Ingrese un numero: " << endl;
+    cin>>x;
+    cout<<endl<<endl;
+    z=x;
+    while(z>=10){//este while tiene como funcion hallar el numeros de digitos del numero ingresado
+        z=z/10;
+        nDigitos++;
+}
+    if(nDigitos==1){
+        char numer [] ={ 48,49,50,51,52,53,54,55,56,57 };
+        int i=0;
+        while(i<10){
+            if (x==i){
+                cout<<numer[i]<<endl;
+            }
+            i++;
+    }
+    }
+    else{
+    cout<<printdig(x,nDigitos)<<endl;
+    }
+}
+
+
+static int printdig(int numero,int nDigitos){
+    char numer [] ={ 48,49,50,51,52,53,54,55,56,57 };
+    int digitoActual=0,i=0;
+
+    for(int j=nDigitos;j>=1;j--){//lo que hace este for es sacar digito por digito mientras a suvez llama la funcion potencia para potenciarlo por el mismo
+
+    digitoActual=numero/(potencia(10,j-1));//se saca el primer digito haciendo una division entera por 10^numero de digitos menos uno
+
+    //y=digitoActual;
+    while(i<10){
+        if (digitoActual==i){
+            cout<<numer[i];
+        }
+        i++;
+        numero= numero%(potencia(10,j-1));//para ir eliminando digitos se hace una division modulo
+    }
+
+    }
+
+}
+
+
+static int potencia(int base, int exp){
+ int ayuda=base;
+    if(exp>1){
+        for(int i=1;i<exp;i++){
+            base = base *ayuda ;
+        }
+        return base;
+    }
+    else{
+        if (exp==1){
+            return base;
+        }else{
+            return 1;
+        }
+    }
+}
---
+++ Problema-9/main.cpp
@@ -0,0 +1,18 @@
+/*Problema 9. Escribir un programa que reciba un número n y lea una cadena de caracteres numéricos, el programa
+debe separar la cadena de caracteres en números de n cifras, sumarlos e imprimir el resultado. En caso de no poderse
+dividir exactamente en números de n cifras se colocan ceros a la izquierda del primer número.
+Ejemplo: Si n=3 y se lee el arreglo 87512395 la suma seria 087+512+395=994.
+Nota: la salida del programa debe ser:
+Original: 87512395.
+Suma: 994.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
